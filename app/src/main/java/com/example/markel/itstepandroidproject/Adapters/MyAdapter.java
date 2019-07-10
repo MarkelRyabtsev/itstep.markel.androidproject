e52a18ab7b05979d9c8576002b46c59b0e27aea7
Markel <m.ryabtsev@outlook.com>
2019-07-10 21:43:02 +0600

    wip

--- app/src/main/java/com/example/markel/itstepandroidproject/Adapters/MyAdapter.java
+++ app/src/main/java/com/example/markel/itstepandroidproject/Adapters/MyAdapter.java
@@ -1,14 +1,31 @@
 package com.example.markel.itstepandroidproject.Adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import com.example.markel.itstepandroidproject.Entities.Tree;
+import com.example.markel.itstepandroidproject.R;
+
+import java.util.ArrayList;
 
 public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    Context context;
+    ArrayList<Tree> arrayListTrees = new ArrayList<>();
+
+    public MyAdapter(Context context, ArrayList<Tree> arrayListTrees){
+        this.context = context;
+        this.arrayListTrees = arrayListTrees;
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tree_recyclerview_tree, viewGroup);
+        return new TreesHolder(view);
     }
 
     @Override
@@ -18,6 +35,13 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     @Override
     public int getItemCount() {
-        return 0;
+        return arrayListTrees.size();
+    }
+
+    public class TreesHolder extends RecyclerView.ViewHolder{
+
+        public TreesHolder(View itemView) {
+            super(itemView);
+        }
     }
 }
